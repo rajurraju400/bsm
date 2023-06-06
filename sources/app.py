4039b5e32721a45392ad6b7f55a567863754cfd4
Aparna Venkatapathiraj <aparnaraj400@gmail.com>
2023-06-06 13:17:26 -0500

    message

--- sources/app.py
+++ sources/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import datetime
 from flask_sqlalchemy import SQLAlchemy
 import json
+from werkzeug.security import generate_password_hash, check_password_hash
+import re
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='Templates')
+app.secret_key = 'your_secret_key'
 
 # Configuring the Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///baby_sleep_tracker.db'
@@ -11,7 +15,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Creating the Database Instance
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+# User Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"User(id={self.id}, username={self.username}, email={self.email})"
+    
+    
 
 class SleepEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,12 +38,99 @@ class SleepEntry(db.Model):
 
     def __repr__(self):
         return f"SleepEntry(id={self.id}, date={self.date}, start_time={self.start_time}, end_time={self.end_time}, notes={self.notes})"
+    
+#Page to render the registration form
 
 
-@app.route('/')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        # Validate form inputs
+        errors = {}
+
+        # Check if username is provided and meets certain criteria
+        if not username:
+            errors['username'] = 'Username is required.'
+        elif len(username) < 4:
+            errors['username'] = 'Username must be at least 4 characters long.'
+
+        # Check if email is provided and is a valid email address
+        if not email:
+            errors['email'] = 'Email is required.'
+        elif not validate_email(email):
+            errors['email'] = 'Invalid email address.'
+
+        # Check if password is provided and meets certain criteria
+        if not password:
+            errors['password'] = 'Password is required.'
+        elif len(password) < 6:
+            errors['password'] = 'Password must be at least 6 characters long.'
+
+        # Check if confirm password matches the password
+        if not confirm_password:
+            errors['confirm_password'] = 'Confirm Password is required.'
+        elif password != confirm_password:
+            errors['confirm_password'] = 'Passwords do not match.'
+
+        if errors:
+            # Render the registration form template with errors
+            return render_template('register.html', errors=errors)
+
+        # Create a new User instance
+        new_user = User(username=username, email=email, password=generate_password_hash(password))
+
+        # Add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Redirect to the login page after successful registration
+        flash('Registration successful. Please continue to log in!!')
+        return redirect(url_for('home'))
+
+    # Render the registration form template
+    return render_template('register.html')
+
+#Page to render the login form from the registration form
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Retrieve the user from the database based on the provided username
+        user = User.query.filter_by(username=username).first()
+        
+        #Check if user exists 
+        if user:
+
+        # Verify the password
+            if user and check_password_hash(user.password, password):
+            # Create a session or token to authenticate the user
+                session['user_id'] = user.id
+
+            # Redirect to the home page after successful login
+                return redirect(url_for('home'))
+        else :
+            return redirect(url_for('register'))
+
+        # Handle invalid login credentials
+        flash('Invalid username or password.')
+
+    # Render the login form template
+    return render_template('login.html')
+
+#To render the home page
+@app.route('/home')
 def home():
     return render_template('home.html')
 
+#To render the page to enter the sleep data 
 
 @app.route('/sleep_entry', methods=['GET', 'POST'])
 def sleep_entry():
@@ -64,6 +167,7 @@ def sleep_entry():
     # Render the sleep entry form template
     return render_template('sleep_entry.html')
 
+#Page to render the sleep history 
 
 @app.route('/sleep_history')
 def sleep_history():
@@ -85,6 +189,21 @@ def sleep_history():
     # Render the sleep history template with the serialized sleep entries
     return render_template('sleep_history.html', sleep_entries=json.dumps(sleep_entries_dict))
 
+#Validate email buil in function to validate the email entered 
+
+
+def validate_email(email):
+    # Email regular expression pattern
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+
+    # Check if the email matches the pattern
+    if re.match(pattern, email):
+        return True
+    else:
+        return False
+
+
+
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3000, debug=True)
+    app.run()
